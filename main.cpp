76c478a88ac3c364ea97459b71d3cd24eb34b60e
Sanskriti9693 <devisunita96935@gmail.com>
2026-05-17 16:19:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 const int N = 9;
 
-// Function to print Sudoku grid
 void printGrid(int grid[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -13,22 +12,22 @@ void printGrid(int grid[N][N]) {
     }
 }
 
-// Check if placing number is safe
+
 bool isSafe(int grid[N][N], int row, int col, int num) {
 
-    // Check row
+
     for (int x = 0; x < N; x++) {
         if (grid[row][x] == num)
             return false;
     }
 
-    // Check column
+
     for (int x = 0; x < N; x++) {
         if (grid[x][col] == num)
             return false;
     }
 
-    // Check 3x3 subgrid
+
     int startRow = row - row % 3;
     int startCol = col - col % 3;
 
@@ -42,13 +41,13 @@ bool isSafe(int grid[N][N], int row, int col, int num) {
     return true;
 }
 
-// Sudoku Solver using Backtracking
+
 bool solveSudoku(int grid[N][N]) {
 
     int row, col;
     bool emptyFound = false;
 
-    // Find empty cell
+
     for (row = 0; row < N; row++) {
         for (col = 0; col < N; col++) {
             if (grid[row][col] == 0) {
@@ -60,22 +59,22 @@ bool solveSudoku(int grid[N][N]) {
 
 FOUND:
 
-    // No empty cell means solved
+
     if (!emptyFound)
         return true;
 
-    // Try numbers 1 to 9
+
     for (int num = 1; num <= 9; num++) {
 
         if (isSafe(grid, row, col, num)) {
 
             grid[row][col] = num;
 
-            // Recursive call
+  
             if (solveSudoku(grid))
                 return true;
 
-            // Backtrack
+    
             grid[row][col] = 0;
         }
     }
