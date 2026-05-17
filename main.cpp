ba3ad72a5b93323da173ad6467d680f324537b0b
sanskriti9693 <sanskriti@sanskritis-MacBook-Air.local>
2026-05-17 15:44:56 +0530

    Improved Sudoku Solver output

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ const int N = 9;
 
 // Function to print Sudoku grid
 void printGrid(int grid[N][N]) {
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cout << grid[i][j] << " ";
         }
         cout << endl;
@@ -17,14 +17,14 @@ void printGrid(int grid[N][N]) {
 bool isSafe(int grid[N][N], int row, int col, int num) {
 
     // Check row
-    for(int x = 0; x < N; x++) {
-        if(grid[row][x] == num)
+    for (int x = 0; x < N; x++) {
+        if (grid[row][x] == num)
             return false;
     }
 
     // Check column
-    for(int x = 0; x < N; x++) {
-        if(grid[x][col] == num)
+    for (int x = 0; x < N; x++) {
+        if (grid[x][col] == num)
             return false;
     }
 
@@ -32,9 +32,9 @@ bool isSafe(int grid[N][N], int row, int col, int num) {
     int startRow = row - row % 3;
     int startCol = col - col % 3;
 
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(grid[startRow + i][startCol + j] == num)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[startRow + i][startCol + j] == num)
                 return false;
         }
     }
@@ -42,37 +42,37 @@ bool isSafe(int grid[N][N], int row, int col, int num) {
     return true;
 }
 
-// Backtracking function
+// Sudoku Solver using Backtracking
 bool solveSudoku(int grid[N][N]) {
 
     int row, col;
     bool emptyFound = false;
 
     // Find empty cell
-    for(row = 0; row < N; row++) {
-        for(col = 0; col < N; col++) {
-            if(grid[row][col] == 0) {
+    for (row = 0; row < N; row++) {
+        for (col = 0; col < N; col++) {
+            if (grid[row][col] == 0) {
                 emptyFound = true;
-                break;
+                goto FOUND;
             }
         }
-        if(emptyFound)
-            break;
     }
 
+FOUND:
+
     // No empty cell means solved
-    if(!emptyFound)
+    if (!emptyFound)
         return true;
 
     // Try numbers 1 to 9
-    for(int num = 1; num <= 9; num++) {
+    for (int num = 1; num <= 9; num++) {
 
-        if(isSafe(grid, row, col, num)) {
+        if (isSafe(grid, row, col, num)) {
 
             grid[row][col] = num;
 
             // Recursive call
-            if(solveSudoku(grid))
+            if (solveSudoku(grid))
                 return true;
 
             // Backtrack
@@ -86,23 +86,28 @@ bool solveSudoku(int grid[N][N]) {
 int main() {
 
     int grid[N][N] = {
-        {3,0,6,5,0,8,4,0,0},
-        {5,2,0,0,0,0,0,0,0},
-        {0,8,7,0,0,0,0,3,1},
-        {0,0,3,0,1,0,0,8,0},
-        {9,0,0,8,6,3,0,0,5},
-        {0,5,0,0,9,0,6,0,0},
-        {1,3,0,0,0,0,2,5,0},
-        {0,0,0,0,0,0,0,7,4},
-        {0,0,5,2,0,6,3,0,0}
+        {5,3,0,0,7,0,0,0,0},
+        {6,0,0,1,9,5,0,0,0},
+        {0,9,8,0,0,0,0,6,0},
+        {8,0,0,0,6,0,0,0,3},
+        {4,0,0,8,0,3,0,0,1},
+        {7,0,0,0,2,0,0,0,6},
+        {0,6,0,0,0,0,2,8,0},
+        {0,0,0,4,1,9,0,0,5},
+        {0,0,0,0,8,0,0,7,9}
     };
 
-    if(solveSudoku(grid)) {
-        cout << "Solved Sudoku:\n";
+    cout << "Unsolved Sudoku:\n";
+    printGrid(grid);
+
+    if (solveSudoku(grid)) {
+
+        cout << "\nSolved Sudoku:\n";
         printGrid(grid);
-    }
-    else {
-        cout << "No solution exists";
+
+    } else {
+
+        cout << "No Solution Exists!";
     }
 
     return 0;
